ef4a00ae72a6f26f8ecddbabd10f350721662431
Tuan Ngo <tuan.23a5@gmail.com>
2021-12-27 11:05:17 +0700

    Main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
+import {ListComponent} from './idioms/list/list.component';
+import {AddComponent} from './idioms/add/add.component';
+import {ItemComponent} from './idioms/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ListComponent,
+    AddComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
